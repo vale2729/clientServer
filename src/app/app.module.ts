a234ccf8382cea1e18a15c5e7e8f5bc2c1aa426f
vale2729 <valentinaaj2729@gmail.com>
2023-04-11 20:37:14 -0500

    proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule,
